27b64ecdfd7ca378794d89c75854e8837d54060c
Matharrr <117796799+Matharrr@users.noreply.github.com>
2023-02-21 13:11:20 +0700

    upload lagi

--- header.h
+++ header.h
@@ -7,6 +7,8 @@
 #define First(L) (L).First
 #define NNama(P) (P)->next_nama
 #define NKota(P) (P)->next_kota
+#define MAXCITY 10
+#define MAXNAME 50
 
 typedef char *infotype;
 typedef struct tNamaList *Naddress;
